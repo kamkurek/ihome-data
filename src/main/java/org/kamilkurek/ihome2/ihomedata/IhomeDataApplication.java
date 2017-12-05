53dccb19cb57fbc8709272ec4aed71546c7c1e08
Kamil Kurek <kamkurek@gmail.com>
2017-12-05 11:05:04 +0100

    init

---
+++ src/main/java/org/kamilkurek/ihome2/ihomedata/IhomeDataApplication.java
@@ -0,0 +1,12 @@
+package org.kamilkurek.ihome2.ihomedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhomeDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhomeDataApplication.class, args);
+	}
+}
